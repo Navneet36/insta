d89b5e0249ff589a03ec4dedeac53ec0d584c582
Samyak <mnavneetbajaj@gmail.com>
2020-07-12 02:26:00 +0530

    try

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
+
 if (process.env.NODE_ENV == "production") {
     app.use(express.static('client/build'))
     const path = require('path')
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
-        console.log(__dirname)
+        res.sendFile(path.resolve('client', 'build', 'index.html'))
     }) 
 }
 
