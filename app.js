3df3142b91481985065ba0332fbd8c0a624766b6
Samyak <mnavneetbajaj@gmail.com>
2020-07-12 01:07:51 +0530

    only console

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV == "production") {
     app.use(express.static('client/build'))
     const path = require('path')
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'))
+        console.log(__dirname)
     }) 
 }
 
